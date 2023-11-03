44276399380795cf9cce4d7bfaa9be098a29c893
nihad-abbasov <nihadhadabbasov2000@gmail.com>
2023-11-03 19:33:40 +0400

    :any added to the ({params}) on page.tsx on template-info/[id]

--- src/app/(product)/template-info/[id]/page.tsx
+++ src/app/(product)/template-info/[id]/page.tsx
@@ -3,7 +3,7 @@
 import { TemplateDetailsPage } from "../../../../components/pageComponents/TemplateDetailsPage/TemplateDetailsPage";
 import { templateDetails } from "../../../../../db";
 
-export default function TemplateDetails({ params }) {
+export default function TemplateDetails({ params }: any) {
   const template = templateDetails.find((temp) => temp.id == params.id);
 
   return <TemplateDetailsPage template={template} />;
