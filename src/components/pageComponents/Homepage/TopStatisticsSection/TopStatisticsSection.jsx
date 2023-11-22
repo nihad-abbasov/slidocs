d9086db34bf323993daef49bf0bdfc66cd1bb469
nihad-abbasov <nihadhadabbasov2000@gmail.com>
2023-11-22 11:56:56 +0400

    TopStatistics component's design updated(small cards done)

--- src/components/pageComponents/Homepage/TopStatisticsSection/TopStatisticsSection.jsx
+++ src/components/pageComponents/Homepage/TopStatisticsSection/TopStatisticsSection.jsx
@@ -4,34 +4,32 @@ import { smallCards, platforms } from "../../../../../db";
 
 export const TopStatisticsSection = () => {
   return (
-    <section className="willFadeIn flex flex-col md:flex-row items-center justify-between">
-      <article className="stat_left w-full md:w-[50%] mb-[1em] md:mb-0">
+    <section className="willFadeIn flex flex-col md:flex-row items-stretch justify-between">
+      <article className="stat_left w-full md:w-[57%] mb-[1em] md:mb-0">
         <Slider />
       </article>
 
-      <article className="stat_right w-full md:w-[45%] grid grid-cols-1 gap-4 justify-content-between">
-        <div className="stat_right_top w-full grid grid-cols-2 gap-4">
+      <article className="stat_right w-full md:w-[40%] grid grid-cols-1 gap-4 justify-content-between">
+        <div className="stat_right_top w-full flex flex-col lg:grid lg:grid-cols-2 gap-4">
           {smallCards.map((card) => {
             return (
-              <div key={card.id} className="card min-h-[230px]">
-                <ul className="py-[2em] px-[1em] flex flex-col items-start justify-between h-full">
+              <div key={card.id} className="card lg:min-h-[230px]">
+                <ul className="lg:py-[2em] px-[1em] grid grid-cols-2 lg:flex lg:flex-col lg:items-start lg:justify-between h-full">
                   <li>
                     <Image
                       src={card.iconPath}
                       width={0}
                       height={0}
                       sizes="100vw"
-                      className="w-full h-full"
+                      className="w-[55%] h-full lg:w-full lg:h-full"
                       alt="Folder"
                     />
                   </li>
-                  <li className="flex flex-col items-start">
+                  <li className="w-full flex flex-col items-start">
                     <span className="font-semibold text-4xl">
                       {card.number}
                     </span>
-                    <span className="text-[#686868] text-sm">
-                      {card.text}
-                    </span>
+                    <span className="text-[#686868] text-sm">{card.text}</span>
                   </li>
                 </ul>
               </div>
