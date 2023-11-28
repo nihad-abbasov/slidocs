05b780a9cd28719dd0c075254df3c5576c9ed98d
nihad-abbasov <nihadhadabbasov2000@gmail.com>
2023-11-28 12:07:19 +0400

    important added to footer mb-[3em]

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import { FooterElement } from "./FooterElement";
 
 export const Footer = () => {
   return (
-    <footer className="footer_unique_container bg-white py-[3em] px-2 rounded-xl mb-[3em]">
+    <footer className="footer_unique_container bg-white py-[3em] px-2 rounded-xl !mb-[3em]">
       <div className="footer_wrapper grid grid-cols-1 md:grid-cols-3 justify-items-center">
         <div className="w-[60%] flex flex-col justify-between items-center md:items-start">
           <Link href="/" className="w-[80%]">
